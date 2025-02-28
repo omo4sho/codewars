3f8e1b01b79b18fcffb441200da10248057e564c
Omoniyi <53401358+omo4sho@users.noreply.github.com>
2025-02-28 19:17:38 +0100

    Update lario_and_muigi_pipe_problem.py

--- 8kyu/lario_and_muigi_pipe_problem.py
+++ 8kyu/lario_and_muigi_pipe_problem.py
@@ -10,3 +10,8 @@ Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
 def pipe_fix(nums):
     return [n for n in range(nums[0], nums[-1] + 1)]
 
+#Solution 2 using a  that starts from the minimum value of the input list and increments by 1 up to the maximum value.
+
+def pipe_fix(nums):
+    return list(range(min(nums), max(nums) + 1))
+
